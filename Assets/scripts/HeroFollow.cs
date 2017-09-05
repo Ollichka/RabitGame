a0492b40543e0a6d4ba021d74f3677b55f3e51f8
Ollichka <salnikova.olia@gmail.com>
2017-09-05 19:38:56 +0300

    add scripts

---
+++ Assets/scripts/HeroFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroFollow : MonoBehaviour
+{
+	public HeroRabit rabit;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Transform rabit_transform = rabit.transform;
+		Transform camera_transform = this.transform;
+		Vector3 rabit_position = rabit_transform.position;
+		Vector3 camera_position = camera_transform.position;
+		camera_position.x = rabit_position.x;
+		camera_position.y = rabit_position.y;
+		camera_transform.position = camera_position;
+	}
+}
+

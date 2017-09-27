84be65561032b4ae0006e2f6759ba669adcf9628
Ollichka <salnikova.olia@gmail.com>
2017-09-28 00:13:57 +0300

    prefabs collectable moving

---
+++ Assets/scripts/Collectable/Bomb.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : Collectable {
+
+	protected override void OnRabitHit(HeroRabit rabit) {
+		if (!rabit.beBig (false)) rabit.die(this.transform);
+		this.CollectedHide();
+	}
+
+}
--- Assets/scripts/HeroRabit.cs
+++ Assets/scripts/HeroRabit.cs
@@ -9,12 +9,25 @@ public class HeroRabit : MonoBehaviour {
 	float JumpTime = 0f;
 	public float MaxJumpTime = 2f;
 	public float JumpSpeed = 2f;
+	public float deathTotalTime = 1f;
 
+
+
+	bool isBig = false;
+	bool isDead = false;
+
+	Transform heroParent = null;
+	SpriteRenderer sr = null;
 	Rigidbody2D myBody = null;
+	Animator animator = null;
 	// Use this for initialization
 	void Start () {
+		sr= GetComponent<SpriteRenderer>();
 		myBody = this.GetComponent<Rigidbody2D> ();
+		animator = this.GetComponent<Animator> ();
+		heroParent = this.transform.parent;
 		LevelController.current.setStartPosition (transform.position);
+
 	}
 	
 	// Update is called once per frame
@@ -31,10 +44,6 @@ public class HeroRabit : MonoBehaviour {
 			vel.x = value * speed;
 			myBody.velocity = vel;
 		}
-
-		Animator animator = GetComponent<Animator> ();
-
-		SpriteRenderer sr = GetComponent<SpriteRenderer>();
 		if (value < 0) {
 			sr.flipX = true;
 			animator.SetBool ("run", true);
@@ -50,13 +59,20 @@ public class HeroRabit : MonoBehaviour {
 		int layer_id = 1 << LayerMask.NameToLayer ("ground");
 		//Перевіряємо чи проходить лінія через Collider з шаром Ground
 		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
-		if(hit) {
-			isGrounded = true;
-		} else {
-			isGrounded = false;
-		}
+
 		//Намалювати лінію (для розробника)
 		Debug.DrawLine (from, to, Color.red);
+		Debug.Log (hit.transform);
+		if(hit) {
+			isGrounded = true;
+			if(hit.transform != null && hit.transform.GetComponent<MovingPlat>() != null){
+				//Приліпаємо до платформи
+				setNewParent(hit.transform);
+			}
+		} else {
+			isGrounded = false;
+			setNewParent (heroParent);
+		}
 
 		//HeroRabit::FixedUpdate
 		//Якщо кнопка тільки що натислась
@@ -83,5 +99,47 @@ public class HeroRabit : MonoBehaviour {
 		} else {
 			animator.SetBool ("jump", true);
 		}
+
+		if (isDead) {
+				LevelController.current.onRabitDeath (this);
+				isDead = false;
+				animator.SetBool ("die", false);
+
+		}
+	}
+
+	void setNewParent(Transform newParent) {
+		if (this.transform.parent != newParent) {
+			//Засікаємо позицію у Глобальних координатах
+			Vector3 pos = this.transform.position;
+
+			//Встановлюємо нового батька
+			this.transform.parent = newParent;
+
+			//Після зміни батька координати кролика зміняться
+			//Оскільки вони тепер відносно іншого об’єкта
+			//повертаємо кролика в ті самі глобальні координати
+			this.transform.position = pos;
+		}
+	}
+
+	public bool beBig(bool big) {
+		if (big && !this.isBig) {
+			this.isBig = true;
+			this.transform.localScale += new Vector3 (0.5f, 0.5f, 0);
+			return true;
+		} else if (!big && this.isBig) {
+			this.isBig = false;
+			this.transform.localScale -= new Vector3 (0.5f, 0.5f, 0);
+			return true;
+		}
+		return false;
+	}
+
+	public void die (Transform die) {
+		if (die!= null) 
+			animator.SetBool ("die", true);
+
+		isDead = true;
 	}
 }
